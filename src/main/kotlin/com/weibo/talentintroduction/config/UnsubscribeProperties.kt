618bb011426ee79b49a17ea3427be44f973b5df8
szwbww <szwbww@163.com>
2026-07-03 17:11:25 +0800

    feat: dns

--- src/main/kotlin/com/weibo/talentintroduction/config/UnsubscribeProperties.kt
+++ src/main/kotlin/com/weibo/talentintroduction/config/UnsubscribeProperties.kt
@@ -1,7 +1,9 @@
 package com.weibo.talentintroduction.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "talent-introduction.unsubscribe")
 data class UnsubscribeProperties(
     val baseUrl: String = "",
